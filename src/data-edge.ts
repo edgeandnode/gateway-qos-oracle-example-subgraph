23fcf02e555fd917a0aeb7e8d73b3382f5a4e863
Craig Tutterow <craig@edgeandnode.com>
2022-09-02 15:26:25 -0500

    Initialize example QoS oracle subgraph that displays json string payload of oracle messages containing IPFS hash of data files

--- src/data-edge.ts
+++ src/data-edge.ts
@@ -1,26 +1,21 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { DataEdge, Message } from "../generated/DataEdge/DataEdge"
-import { ExampleEntity } from "../generated/schema"
+import { BigInt, Bytes } from "@graphprotocol/graph-ts"
+import { DataEdge, SubmitQoSPayloadCall } from "../generated/DataEdge/DataEdge"
+import { OracleMessage } from "../generated/schema"
 
-export function handleMessage(event: Message): void {
+export function handleSubmitQoSPayload(call: SubmitQoSPayloadCall): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = OracleMessage.load(call.transaction.hash.toHexString())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
   if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+    entity = new OracleMessage(call.transaction.hash.toHexString())
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
   // Entity fields can be set based on event parameters
-  entity.data = event.params.data
+  entity.payload = call.inputs._payload.toString();
+  entity.createdAt = call.block.timestamp;
 
   // Entities can be written to the store with `.save()`
   entity.save()
